d920332d5ed20160337819b44d08558f6bf4bbdb
Mohanbg <mohanavel2299@gmail.com>
2024-05-03 14:40:48 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/backgroundapp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.backgroundapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Color Changing App'),
+        ),
+        body: const HomePage(),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color _colors = Colors.white;
+
+  void _changeColor() {
+    setState(() {
+      _colors = Color((math.Random().nextDouble() * 0xFFFFFF).toInt() << 0)
+          .withOpacity(1.0);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: _colors,
+      child: Center(
+        child: ElevatedButton(
+          onPressed: _changeColor,
+          child: const Text('Change Background Color'),
+        ),
+      ),
+    );
+  }
+}
